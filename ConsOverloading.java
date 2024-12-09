5ca3a0cc880ef91cfb453a89380d5bc551cf221a
Yuvraj Gajanan Budhwat <yuvrajbudhwat@gmail.com>
2024-12-09 22:02:53 +0530

    Add files via upload

--- ConsOverloading.java
+++ ConsOverloading.java
@@ -1,39 +1,19 @@
-package constructor.example1;
+package constructor.example9;
 
 public class ConsOverloading {
-	
-	int value1;
-	int value2;
+	private int rollno;
 	
 	ConsOverloading(){
-		value1=20;
-		value2=30;
-		System.out.println("Inside 1st Constructor");
+		rollno=100;
 	}
-	ConsOverloading(int a){
-		value1=a;
-		System.out.println("Inside 2nd Constructor");
+	ConsOverloading(int rno){
+		this();
+		rollno = rollno + rno;
 	}
-	ConsOverloading(int a, int b){
-		value1=a;
-		value2=b;
-		System.out.println("Inside 3rd Constructor");
+	public int getRollNo() {
+		return rollno;
 	}
-	public void display() {
-		
-		System.out.println();
-		System.out.println("Value1 ="+" "+value1);
-		System.out.println("Value2 ="+" "+value2);
-	}
-	
-	public static void main(String[] args) {
-		
-		ConsOverloading d1=new ConsOverloading();
-		ConsOverloading d2=new ConsOverloading();
-		ConsOverloading d3=new ConsOverloading();
-		
-		d1.display();
-		d2.display();
-		d3.display();
+	public void setRollNo(int rollno) {
+		this.rollno=rollno;
 	}
 }
---
+++ Test.java
@@ -0,0 +1,9 @@
+package constructor.example9;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		ConsOverloading c1=new ConsOverloading(12);
+		System.out.println(c1.getRollNo());
+	}
+}
